88d4ed5ec42dfb43ebd1555246649773c890d15e
Kaybee <passionno1@qq.com>
2022-10-28 22:04:46 +0800

    Update download_bilibili_audio.py

--- download_bilibili_audio.py
+++ download_bilibili_audio.py
@@ -23,27 +23,34 @@ def extract_audio(code, audio_name='%(title)s'):
 
 
 def cut_audio(input_path):
-    """ use ffmpeg to cut audio """
+    """ use ffmpeg to cut audio first 10 seconds"""
     output_path = Path(input_path).parent.joinpath("out")
     output_path.mkdir(parents=True, exist_ok=True)
-    output = str(output_path / input_path)
+    output = str(output_path.absolute() / input_path)
     command = f"ffmpeg -ss 00:00:10 -i {input_path} {output}"
     print(command)
     os.system(command)
 
 
-method_name = sys.argv[1]
+if __name__ == "__main__":
+    arg_lens = len(sys.argv)
+    if arg_lens < 2 or arg_lens > 3:
+        raise RuntimeError("Invalid number of args. Please use right arguments.")
 
-if method_name == "cut":
-    print("do cut job.")
-    input_file = sys.argv[2]
-    cut_audio(input_file)
-elif method_name == "download":
-    print("do download job.")
-    av_no = sys.argv[2]
-    name = sys.argv[3]
-    extract_audio(av_no, name)
-else:
-    print(f"invalid argument: {method_name}.")
-
-print("Finish.")
+    method_name = sys.argv[1]
+    if method_name == "-c" or method_name == "--cut":
+        print("do cut job.")
+        input_file = sys.argv[2]
+        cut_audio(input_file)
+    elif method_name == "-dc":
+        print("download and cut first 10 seconds.")
+        av_no = sys.argv[1]
+        name = sys.argv[2]
+        file_name = extract_audio(av_no, name)
+        cut_audio(file_name)
+    else:
+        av_no = sys.argv[1]
+        name = sys.argv[2]
+        print("do download job.")
+        file_name = extract_audio(av_no, name)
+    print("Finished.")
